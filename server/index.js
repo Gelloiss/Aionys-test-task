97896b71fb78d63fb8e1e4a118f349948199f558
Gelloiss <certain@yourcompany.best>
2020-09-02 16:09:18 +0300

    realised adding to base

--- server/index.js
+++ server/index.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.raw({extended: true}));
 const fs = require('fs');
 const path = require('path');
 
+/*FUNCTIONS*/
+const getBase = (file = 'note.json') => {
+  return JSON.parse(fs.readFileSync(path.resolve(__dirname, 'base', file)));
+}
+
+const setBase = (file, data) => {
+  fs.writeFileSync((path.resolve(__dirname, 'base', file)), JSON.stringify(data));
+  return true;
+}
+
+/*ROUTING*/
 app.get('/', (request, response) => {
   response.send(0);
-})
+});
 
 app.get('/notes/:id?', (request, response) => { //Get notes
   let error = false;
   let result = {};
   const id = request.params.id;
-  const notes = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'base', 'note.json')));
+  const notes = getBase();
   if (id === undefined) { //get all notes
     result = {
       notes: notes,
@@ -43,11 +58,24 @@ app.get('/notes/:id?', (request, response) => { //Get notes
     };
   }
   response.send(JSON.stringify(result));
-})
+});
 
-app.listen(port, (err) => {
+app.post('/notes', (request, response) => {
+  let notes = getBase();
+  let i = getBase('increment.json').note;
+  i++;
+  notes.push({
+    'id':i,
+    'text':request.body.text
+  });
+  setBase('note.json', notes);
+  setBase('increment.json', {'note':i});
+  response.send(true);
+});
+
+app.listen(port,(err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
   console.log(`server is listening on ${port}`)
-})
+});
